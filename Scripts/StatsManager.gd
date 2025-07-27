e639d12a763b3cca6cac71491babd286ea184b05
Ri <7138505+BlustrawberRi@users.noreply.github.com>
2025-07-27 18:54:47 +0200

    Fix stat bars working.

--- Scripts/StatsManager.gd
+++ Scripts/StatsManager.gd
@@ -3,15 +3,19 @@ extends Node
 enum stats {hungry, sleepy, boredom}
 
 var stats_dict := {
-	stats.hungry : 0.0,
-	stats.sleepy : 0.0,
-	stats.boredom : 0.0
+	stats.hungry : 100.0,
+	stats.sleepy : 90.0,
+	stats.boredom : 50.0
 }
 
-signal stat_change(stat, change)
+signal stat_change(stat, value)
 
 func update_stat(stat: stats, change: float):
-	stats_dict.set(stat, stats_dict.get(stat) + change)
-	stat_change.emit(stat, change)
+	var new_value = stats_dict.get(stat) + change
+	stats_dict.set(stat, new_value)
+	stat_change.emit(stat, new_value)
 	print("Stat: ", stat, " Value: ", stats_dict.get(stat))
+
+func get_stat_value(stat:stats) -> int:
+	return stats_dict.get(stat)
 	
--- Scripts/stat_bar.gd
+++ Scripts/stat_bar.gd
@@ -5,8 +5,12 @@ var stat: StatsManager.stats
 
 func _ready():
 	StatsManager.stat_change.connect(update)
+	init()
 	
 
 func update(changed_stat: StatsManager.stats, new_value: float):
 	if(changed_stat != stat): return
 	value = new_value
+
+func init():
+	value = StatsManager.get_stat_value(stat)
