ed4fc975cdd867e3fc571285b6d15299cf6fddaa
Ænthroppe <jamison.bassett@gmail.com>
2025-08-18 19:49:56 +0200

    basic end result calculation

--- Scripts/StatsManager.gd
+++ Scripts/StatsManager.gd
@@ -10,12 +10,21 @@ var stats_dict := {
 	stats.crime : 100.0,
 }
 
+var stats_changed := {
+	stats.boredom : 0.0,
+	stats.thirst : 0.0,
+	stats.anger : 0.0,
+	stats.money : 0.0,
+	stats.crime : 0.0,
+}
+
 signal stat_change(stat, value)
 
 func update_stat(stat: stats, change: float):
 	var new_value = max(0, stats_dict.get(stat) + change)
 	stats_dict.set(stat, new_value)
 	stat_change.emit(stat, new_value)
+	stats_changed.set(stat, stats_changed.get(stat) + 1)
 	print("Stat: ", stat, " Value: ", stats_dict.get(stat))
 
 func get_stat_value(stat:stats) -> int:
---
+++ Scripts/end_game.gd
@@ -0,0 +1,65 @@
+extends Button
+
+
+var possible_results = [
+	"nerd", "depressed", "thirst_trap",
+]
+
+var result_stats = {
+	"nerd" : {
+	StatsManager.stats.boredom : 10.0,
+	StatsManager.stats.thirst : 2.0,
+	StatsManager.stats.anger : 5.0,
+	StatsManager.stats.money : 0.0,
+	StatsManager.stats.crime : 0.0
+	},
+	
+	"depressed" : {
+	StatsManager.stats.boredom : 0.0,
+	StatsManager.stats.thirst : 0.0,
+	StatsManager.stats.anger : 5.0,
+	StatsManager.stats.money : 10.0,
+	StatsManager.stats.crime : 0.0
+	},
+	
+	"thirst_trap": {
+	StatsManager.stats.boredom : 2.0,
+	StatsManager.stats.thirst : 10.0,
+	StatsManager.stats.anger : 5.0,
+	StatsManager.stats.money : 0.0,
+	StatsManager.stats.crime : 0.0,
+	}
+	
+}
+
+var visible_after_item_uses = 5
+var current_item_uses = 0
+
+func _ready():
+	self.visible = false
+	StatsManager.stat_change.connect(check_set_visible)
+
+func check_set_visible():
+	current_item_uses += 1
+	if (current_item_uses >= visible_after_item_uses):
+		self.visible = true
+
+func calc_result():
+	var absolute_distance = 999
+	var target = ""
+	for result in possible_results:
+		print("checking distance to ", result)
+		var local_distance = 0
+		for stat in result_stats.get(result):
+			local_distance += abs(StatsManager.stats_changed.get(stat) - result_stats.get(result).get(stat))
+		if (local_distance <= absolute_distance):
+			print("closer target found! ", result, local_distance)
+			absolute_distance = local_distance
+			target = result
+	print("the joshkagotchi will be ", target)
+	return target
+	
+
+func _on_pressed():
+	calc_result()
+	pass # Replace with function body.
