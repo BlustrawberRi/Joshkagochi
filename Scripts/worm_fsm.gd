2d247774b47201925ba7b3d0e9fac80e8c26581e
Ænthroppe <jamison.bassett@gmail.com>
2025-08-22 01:20:08 +0200

    worm can turn when walking now

--- Scripts/worm_fsm.gd
+++ Scripts/worm_fsm.gd
@@ -105,6 +105,12 @@ func idle():
 	target_x = clampi(target_x, 560, 991)
 	var target_y = position.y + randi_range(-50, 50) * 2
 	target_y = clampi(target_y, 356, 594)
+	if(target_x <= self.position.x):
+		body.flip_h = false
+		face.flip_h = false
+	else:
+		body.flip_h = true
+		face.flip_h = true
 	
 	var target_destination = Vector2(target_x, target_y)
 	tween = get_tree().create_tween()
